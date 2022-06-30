2500cfbff23e7bf8a15cbea5dc454f97a253eb1e
christian <cristhho@gmail.com>
2022-06-29 20:33:22 -0500

    home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
-import { Typography } from '@mui/material';
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
 import type { NextPage } from 'next';
 
 import ShopLayout from '../components/layouts/ShopLayout';
+import { initialData } from '../database/products';
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +12,24 @@ const Home: NextPage = () => {
     >
       <Typography variant='h1' component='h1'>Inicio</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
+
+      <Grid container spacing={4}>
+        {
+          initialData.products.map((product) => (
+            <Grid key={product.slug} item xs={6} sm={4}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component='img'
+                    image={`products/${product.images[0]}`}
+                    alt={product.title}
+                  />
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))
+        }
+      </Grid>
     </ShopLayout>
   )
 }
