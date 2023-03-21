66f243e37c2940b1c4d44a200ed1b526878b982d
christian <cristhho@gmail.com>
2023-03-21 14:45:45 -0500

    order schema

---
+++ interfaces/order.ts
@@ -0,0 +1,26 @@
+import { IAddress } from './cart';
+import { IUser } from './user';
+
+export interface IOrder {
+  _id: string;
+  user: IUser | string;
+  orderItems: IOrderItem[];
+  shippingAddress: IAddress;
+  paymentResult?: string;
+  numberOfItems: number;
+  subTotal: number;
+  tax: number;
+  total: number;
+  isPaid: boolean;
+  paidAt?: string;
+}
+
+export interface IOrderItem {
+  _id: string;
+  title: string;
+  size: string;
+  quantity: number;
+  slug: string;
+  image: string;
+  price: number;
+}
