eb12403ecc7e9b09a000804dd71dddefbe4ac73f
christian <cristhho@gmail.com>
2023-12-24 18:41:41 -0500

    seed database, create products and images

--- src/seed/seed-db.ts
+++ src/seed/seed-db.ts
@@ -2,16 +2,38 @@ import { initialData } from "./seed";
 import prisma from "../lib/prisma";
 
 async function main() {
-  await Promise.all([
-    prisma.productImage.deleteMany(),
-    prisma.product.deleteMany(),
-    prisma.category.deleteMany(),
-  ]);
+  await prisma.productImage.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
 
   const { categories, products } = initialData;
   await prisma.category.createMany({
     data: categories.map((name) => ({ name }))
   });
+
+  const categoriesDB = await prisma.category.findMany();
+  const categoriesMap = categoriesDB.reduce((map, category) => {
+    map[category.name.toLowerCase()] = category.id
+    return map;
+  }, {} as Record<string, string>);
+
+  products.forEach(async (product) => {
+    const {images, type, ...$product} = product;
+    const dbProduct = await prisma.product.create({
+      data: {
+        ...$product,
+        categoryId: categoriesMap[type.toLowerCase()]
+      }
+    });
+    const $images = images.map((image) => ({
+      url: image,
+      productId: dbProduct.id
+    }));
+
+    await prisma.productImage.createMany({
+      data: $images
+    })
+  })
 
   console.log("seed executed. Check database!!");
 }
