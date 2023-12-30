da7ac0243a86447c5119a913c0aa985b8ec1aff5
christian <cristhho@gmail.com>
2023-12-29 21:25:13 -0500

    show total items in cart

--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -5,12 +5,18 @@ import { CartProduct } from '@/domain/model';
 
 interface CartState {
   cart: CartProduct[];
+
+  getTotalItems: () => number;
 }
 
 export const useCartStore = create<CartState>()(
   persist<CartState>(
-    () => ({
-      cart: []
+    (_, get) => ({
+      cart: [],
+      getTotalItems: () => {
+        const { cart } = get()
+        return cart.reduce((total, item) => total + item.quantity, 0);
+      }
     }),
     {
       name: "shopping-cart",
