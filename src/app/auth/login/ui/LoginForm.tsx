ba6f485b83679af444819844b081a1f16b2dd2a2
christian <cristhho@gmail.com>
2024-01-04 00:08:28 -0500

    login with credentials

---
+++ src/app/auth/login/ui/LoginForm.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import Link from 'next/link';
+import { useFormState, useFormStatus } from 'react-dom';
+
+import { authenticate } from '@/lib/actions';
+
+export const LoginForm = () => {
+  const [errorMessage, dispatch] = useFormState(authenticate, undefined);
+
+  console.log(errorMessage)
+
+  return (
+    <form className='flex flex-col' action={dispatch}>
+      <label htmlFor='email'>Correo electrónico</label>
+      <input
+        className='px-5 py-2 border bg-gray-200 rounded mb-5'
+        id='email'
+        name='email'
+        type='email'
+      />
+
+      <label htmlFor='password'>Contraseña</label>
+      <input
+        className='px-5 py-2 border bg-gray-200 rounded mb-5'
+        id='password'
+        name='password'
+        type='password'
+      />
+
+      <button className='btn-primary' type='submit'>Ingresar</button>
+
+      {/* divisor l ine */}
+      <div className='flex items-center my-5'>
+        <div className='flex-1 border-t border-gray-500'></div>
+        <div className='px-2 text-gray-800'>O</div>
+        <div className='flex-1 border-t border-gray-500'></div>
+      </div>
+
+      <Link href='/auth/new-account' className='btn-secondary text-center'>
+        Crear una nueva cuenta
+      </Link>
+    </form>
+  );
+};
