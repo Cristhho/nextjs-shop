5916f501e6eebe4186c1d8eb3fd36d2088114da2
christian <cristhho@gmail.com>
2024-01-06 18:53:00 -0500

    logout user

---
+++ src/app/(shop)/profile/page.tsx
@@ -0,0 +1,21 @@
+import { redirect } from 'next/navigation';
+
+import { auth } from '@/auth.config';
+import { Title } from '@/components';
+
+export default async function ProfilePage() {
+  const session = await auth()
+
+  if (!session?.user) redirect('/auth/login')
+
+  return(
+    <div>
+      <Title title='Perfil' />
+      <pre>
+        {
+          JSON.stringify(session.user, null, 2)
+        }
+      </pre>
+    </div>
+  )
+}
