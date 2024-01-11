566a682fbab14c52362fed2e10cb1ef94ca836ec
christian <cristhho@gmail.com>
2024-01-11 00:03:12 -0500

    order prisma model

--- src/seed/seed-db.ts
+++ src/seed/seed-db.ts
@@ -4,6 +4,9 @@ import { initialData } from './seed';
 import { countries } from './seed-countries';
 
 async function main() {
+  await prisma.orderAddress.deleteMany();
+  await prisma.orderItem.deleteMany();
+  await prisma.order.deleteMany();
   await prisma.productImage.deleteMany();
   await prisma.product.deleteMany();
   await prisma.category.deleteMany();
